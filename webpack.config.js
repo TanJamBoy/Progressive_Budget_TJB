27e0393173451f5a0f2b872bc6c38cd465ae13d8
Tanner Boyer <tannerjboyer@gmail.com>
2020-09-25 03:18:01 -0700

    deploying finished application

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+module.exports = {
+    entry: "./public/index.js",
+    output: {
+        path: `${__dirname}/public/dist`,
+        filename: "bundle.js"
+    },
+    mode: "development",
+    plugins: [
+        new WebpackPwaManifest({
+          name: 'Budget App',
+          short_name: 'Budget App',
+          background_color: '#ffffff',
+          theme_color: "#ffffff",
+          start_url: "/",
+          display: "standalone",
+          inject: false,
+          fingerprints: false,
+          icons: [
+            {
+              src: path.resolve('public/icons/icon-512x512.png'),
+              sizes: [192, 512]
+            }
+          ]
+        })
+    ]
+};
